343ef301297bef1ccdcaec2d231895f25823f945
Tauha Shaik <tauhashaik@Tauhas-MacBook-Air.local>
2023-11-23 01:35:31 +0200

    completed

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,17 @@
+// Create Array
+let boolArray = [true, true, false, true, false, true, false, true, false];
+
+// Creating Variables for button and display
+let resultInput = document.getElementById('resultInput');
+let calcbtn = document.getElementById('calcbtn');
+
+// addEventListener for button functionality
+calcbtn.addEventListener('click', trueCount);
+
+function trueCount() {
+    let trueValues = boolArray.filter(value => value === true).length;
+
+    // Display the result in the input field
+    resultInput.value = trueValues;
+}
+
